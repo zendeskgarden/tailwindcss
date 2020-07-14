2e5c24f27df6d08f8a8aa165f7470928bef0ea17
Austin Green <agreen@zendesk.com>
2020-07-14 15:07:28 -0700

    Fix focus-visible in production

--- example/craco.config.js
+++ example/craco.config.js
@@ -4,6 +4,7 @@ module.exports = {
       plugins: [
         require("tailwindcss"),
         require("postcss-focus-visible")({
+          preserve: false,
           replaceWith: ":global(.focus-visible)", // necessary for CSS modules support
         }),
         require("autoprefixer"),
