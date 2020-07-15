8a0599e1439694f4dff64754f4eea73c625de811
Austin Green <agreen@zendesk.com>
2020-07-15 14:31:37 -0700

    Remove new spacing theme values

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -2,11 +2,6 @@ module.exports = {
   purge: ['./src/**/*.jsx', './src/**/*.tsx'],
   theme: {
     extend: {
-      spacing: {
-        '1-1/2': '6px',
-        '13-1/2': '54px',
-        17: '68px'
-      },
       borderRadius: theme => ({
         2: theme('spacing.2'),
         4: theme('spacing.4')
