a39fa1ebefffb001f8bca1738702b064b66217ba
Austin Green <agreen@zendesk.com>
2020-07-22 10:48:38 -0700

    Updates [skip ci]

---
+++ precache-manifest.bfcd25154ef18c9252b9b4a60114c976.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f911c35a42b379e9bc11e301d46fcce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "259fce51be5be14510b7",
+    "url": "./static/css/main.47f7be91.chunk.css"
+  },
+  {
+    "revision": "e8924d1388cf42581168",
+    "url": "./static/js/2.f645c54e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.f645c54e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259fce51be5be14510b7",
+    "url": "./static/js/main.3be4a8be.chunk.js"
+  },
+  {
+    "revision": "b12fb06c1b19e2084157",
+    "url": "./static/js/runtime-main.e89f0be9.js"
+  },
+  {
+    "revision": "9208978ebdc300e78bc4af0c7d84ee20",
+    "url": "./static/media/answer-bot.9208978e.svg"
+  },
+  {
+    "revision": "f1b3314a99924e4e17f685e32c59b227",
+    "url": "./static/media/wordmark-zendesk.f1b3314a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.164efa115c9606acbab50b3eb1fa7e59.js"
+  "./precache-manifest.bfcd25154ef18c9252b9b4a60114c976.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
