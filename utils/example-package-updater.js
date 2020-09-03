17ed7b13e8c6a7b10c9e4642069e51477c67f2d2
Austin Green <agreen@zendesk.com>
2020-09-03 11:49:18 -0700

    fix(build): allow standard-version deployments to update all yarn workspaces

---
+++ utils/example-package-updater.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+const stringifyPackage = require('stringify-package');
+const detectIndent = require('detect-indent');
+const detectNewline = require('detect-newline');
+
+module.exports.readVersion = function (contents) {
+  return JSON.parse(contents).dependencies['@zendeskgarden/tailwindcss'];
+};
+
+module.exports.writeVersion = function (contents, version) {
+  const json = JSON.parse(contents);
+  const indent = detectIndent(contents).indent;
+  const newline = detectNewline(contents);
+
+  json.dependencies['@zendeskgarden/tailwindcss'] = version;
+
+  return stringifyPackage(json, indent, newline);
+};
+
+module.exports.isPrivate = function (contents) {
+  return JSON.parse(contents).private;
+};
